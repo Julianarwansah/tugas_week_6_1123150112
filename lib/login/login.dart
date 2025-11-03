57de7548ebcf4a635c6efdd566bbf8f54510d0d8
NamaAnda <julianarwansahh@gmail.com>
2025-11-03 08:56:06 +0700

    Menambahkan folder dan file login.dart2

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 
-class JulLogin extends StatelessWidget {
+class JulLogin extends StatefulWidget {
   const JulLogin({super.key});
 
   @override
+  State<JulLogin> createState() => _JulLoginState();
+}
+
+class _JulLoginState extends State<JulLogin> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      backgroundColor: Colors.white,
+      body: const Center(
         child: Text(
           "Halaman Login",
-          style: TextStyle(
-            fontSize: 18,
-            color: Colors.grey,
-          ),
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
         ),
       ),
     );
